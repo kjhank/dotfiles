75ff335b6b2e0988202f18b7e5da3f7f4ccc5026
Krzysztof Hankiewicz <khankiewicz@cleeng.com>
2025-07-31 11:06:40 +0200

    fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+alias dotfiles='/usr/bin/git --git-dir=/Users/kjhank/.dotfiles/ --work-tree=/Users/kjhank'
+
+# starship init fish | source
+
+
+
+# pnpm
+set -gx PNPM_HOME "/Users/kjhank/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
